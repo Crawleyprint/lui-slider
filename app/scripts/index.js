cfeb02325691b0beee101200e893537887a558ca
Mladen Stepanic <crawleyprint@gmail.com>
2015-02-08 15:10:16 +0100

    Initial re-render with javascript

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -7,17 +7,23 @@
    *
    * @param {Object} o - Slider options map
    */
-  function LuiSlider(elementName) {
+  function LuiSlider(options) {
     /**
      * first, store reference to DOM node that is going to house our slider
      */
-    this.el = document.getElementById(elementName);
+    this.el = document.getElementById(options.elementId);
+    this.activeClass = options.activeClass || 'lui-slider__item--active';
     /**
      * ... then, see what's already in there and store an instance variable
      * with list of those images and basic attributes
      */
-    this.images = this.setup();
+    this.setup();
 
+    /**
+     * Remove all images to make scene for template rendering
+     */
+    this.clean();
+    this.render();
     /**
      * ... and return slider object to allow chaining of methods
      */
@@ -27,17 +33,59 @@
   LuiSlider.prototype.setup = function luiGatherFacts() {
     var loadedImages = [];
     var images = this.el.getElementsByTagName('img');
+    var that = this;
+
 
     Array.prototype.slice.call(images).forEach(function addImageToInitial(img) {
+      var isActive = img.classList.contains(that.activeClass);
       loadedImages.push({
         src: img.getAttribute('src'),
-        alt: img.getAttribute('alt')
+        alt: img.getAttribute('alt'),
+        isActive: isActive
       });
     });
 
-    return loadedImages;
+    this.images = loadedImages;
+
+    return this;
   };
 
-  var mainSlider = new LuiSlider('slider');
+  LuiSlider.prototype.clean = function() {
+    var element = this.el;
+    while(element.hasChildNodes()) {
+      element.removeChild(element.firstChild);
+    }
+    return this;
+  };
+
+  /**
+   * Makes default template if no template string is provided to constructor
+   *
+   * @param {Object} item - JSON object for image to render
+   */
+  LuiSlider.prototype.template = function(item) {
+    var activeClass = item.isActive ? ' lui-slider__item--active': '';
+    return [
+      '<div class="lui-slider__item' + activeClass + '">',
+        '<img ',
+          'class="lui-slider__image" ',
+          'src="' + item.src + '" alt="' + item.alt + '">',
+      '</div>'
+    ].join('');
+  };
+
+  LuiSlider.prototype.render = function() {
+    var html = [];
+    var that = this;
+    this.images.forEach(function(image) {
+      html.push(that.template(image));
+    });
+    this.el.innerHTML = html.join('');
+    return this;
+  };
+
+  var mainSlider = new LuiSlider({
+    elementId: 'slider'
+  });
 
 })(window);
