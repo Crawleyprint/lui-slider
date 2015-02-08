961219796b00ab3bca7ba54734b565e28198f59d
Mladen Stepanic <crawleyprint@gmail.com>
2015-02-08 12:46:25 +0100

    Adds css compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 'use strict';
 
-global.gulp = require('gulp');
+var gulp = require('gulp');
 
 var sass = require('gulp-sass');
 var server = require('gulp-webserver');
@@ -8,11 +8,28 @@ var server = require('gulp-webserver');
 /**
  * Initialize development web server with live reload support.
  */
-gulp.task('webserver', function setupWebserver() {
+gulp.task('webserver', ['watch', 'sass'], function setupWebserver() {
   gulp.src('app/')
     .pipe(server({
       livereload: true
     }));
+});
+
+/**
+ * Uses gulp to compile css with node-sass.
+ */
+gulp.task('sass', function compileSass() {
+  gulp.src('app/sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css/'));
+});
+
+/**
+ * Set up file system watchers to trigger certain gulp task when certain
+ * type of files have been changed.
+ */
+gulp.task('watch', function setupWatchers() {
+  gulp.watch('app/sass/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['webserver']);
