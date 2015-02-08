9d5113cee0e71475a59cb1d0e469e1bb9e04e272
Mladen Stepanic <crawleyprint@gmail.com>
2015-02-08 13:54:04 +0100

    Base slider object constructor

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,5 +1,43 @@
 'use strict';
 
-(function setupLUISlider(window, undefined) {
-  console.log('calling from function');
+;(function setupLUISlider(window, undefined) {
+
+  /**
+   * Slider app prototype
+   *
+   * @param {Object} o - Slider options map
+   */
+  function LuiSlider(elementName) {
+    /**
+     * first, store reference to DOM node that is going to house our slider
+     */
+    this.el = document.getElementById(elementName);
+    /**
+     * ... then, see what's already in there and store an instance variable
+     * with list of those images and basic attributes
+     */
+    this.images = this.setup();
+
+    /**
+     * ... and return slider object to allow chaining of methods
+     */
+    return this;
+  }
+
+  LuiSlider.prototype.setup = function luiGatherFacts() {
+    var loadedImages = [];
+    var images = this.el.getElementsByTagName('img');
+
+    Array.prototype.slice.call(images).forEach(function addImageToInitial(img) {
+      loadedImages.push({
+        src: img.getAttribute('src'),
+        alt: img.getAttribute('alt')
+      });
+    });
+
+    return loadedImages;
+  };
+
+  var mainSlider = new LuiSlider('slider');
+
 })(window);
