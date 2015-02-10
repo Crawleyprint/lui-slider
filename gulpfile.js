2a02b2a8920beecbae2ff71afa5cfbf38df458d3
Mladen Stepanic <crawleyprint@gmail.com>
2015-02-10 23:59:45 +0100

    Adds markdown to html for demo page

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,20 @@ var sass = require('gulp-sass');
 var server = require('gulp-webserver');
 var autoprefixer = require('gulp-autoprefixer');
 var jshint = require('gulp-jshint');
+var markdown = require('gulp-markdown');
+var fileinclude = require('gulp-file-include');
+var rename = require('gulp-rename');
 
 /**
  * Initialize development web server with live reload support.
  */
-gulp.task('webserver', ['watch', 'sass', 'jshint'], function setupWebserver() {
-  gulp.src('app/')
-    .pipe(server({
-      livereload: true
-    }));
-});
+gulp.task('webserver', ['fileinclude', 'watch', 'sass', 'jshint'],
+  function setupWebserver() {
+    gulp.src('app/')
+      .pipe(server({
+        livereload: true
+      }));
+  });
 
 /**
  * Uses gulp to compile css with node-sass.
@@ -27,6 +31,26 @@ gulp.task('sass', function compileSass() {
       browsers: ['last 2 versions', 'ie 9']
     }))
     .pipe(gulp.dest('app/css/'));
+});
+
+/**
+ * Pipe README.md into HTML
+ */
+gulp.task('markdown', function convertMarkdown() {
+  gulp.src('./README.md')
+    .pipe(markdown())
+    .pipe(rename('readme.html'))
+    .pipe(gulp.dest('./app/includes'));
+});
+
+/**
+ * Include partials into html files
+ */
+gulp.task('fileinclude', ['markdown'], function includeFile() {
+  gulp.src(['app/index.html'])
+    .pipe(fileinclude())
+    .pipe(rename('dev.html'))
+    .pipe(gulp.dest('./app'));
 });
 
 /**
@@ -45,6 +69,8 @@ gulp.task('jshint', function setupJSHint() {
 gulp.task('watch', function setupWatchers() {
   gulp.watch('app/sass/**/*.scss', ['sass']);
   gulp.watch('app/scripts/**/*.js', ['jshint']);
+  gulp.watch('README.md', ['markdown', 'fileinclude']);
+  gulp.watch('index.html', ['fileinclude']);
 });
 
 gulp.task('default', ['webserver']);
